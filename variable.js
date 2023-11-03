975b0edac5c5d2a4a8eb87df2c1d021b1067df3d
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-03 00:45:38 -0700

    this is variable part of the javascript

---
+++ variable.js
@@ -0,0 +1,19 @@
+const accountId=144542
+let accountEmail="pramod@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId=2//const in is not valid because can't change the const variable
+
+accountEmail="pk@gmail.com"
+accountPassword="121211212"
+accountCity="Sikar"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+/*
+prefer not use the var
+bacause scope matter
+
+*/
