3320d710666744f4aeeef331555bbee8c7f73e66
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-04 03:10:11 -0700

    this is conversion of the data type

---
+++ conversion.js
@@ -0,0 +1,30 @@
+let score="33abc"
+
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"---> 33
+//"33abc"---NaN
+//true----1 and false----0
+
+//conversion
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1----true
+//0----false
+//""----false
+//"val"----true
+
+
+//convert the number to string
+
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
