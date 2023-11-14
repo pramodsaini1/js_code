b0f4679be307005bc3ca405466842a81295cf176
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-14 04:12:39 -0800

    type of the data types primitive and non-primitive also classified

---
+++ dataty.js
@@ -0,0 +1,36 @@
+// primitive
+
+// 7 Types-----String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+//Reference(Non-Primitive)
+
+//Array,Object,Functions
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false//return type------boolean
+const outsideTemp=null//return type-------object
+
+const id=Symbol('123')
+
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+
+const BigNumber=32434543538n//return type-----number
+
+const heros=["shaktiman","naagra","doga"];
+//return type-------object
+let myobj={
+    name:"pramod",
+    age:22,
+}//return type-----object
+
+const myFunction=function(){
+    console.log("Hello World");
+}//return type----function
+
+
+
