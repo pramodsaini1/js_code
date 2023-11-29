cfe752df9085f76434793e699981ca0b2f41673c
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-29 01:10:44 -0800

    this is concept of the scopes of the variables

---
+++ scopes.js
@@ -0,0 +1,51 @@
+ let a=300
+
+if(true){
+    let a=10
+    const b=20
+    console.log(a);
+ }
+
+ 
+
+
+console.log(a);
+//console.log(b);
+
+
+function one(){
+    const username="pramod"
+
+    function two(){
+        const website="youtube"
+       // console.log(username);
+    }
+   // console.log(website);
+   two()
+
+}
+one()
+
+if(true){
+   const username="prmaod"
+   if(username=="prmaod"){
+      const wensite="  youtube"
+      //console.log(username+wensite);
+   }
+   //console.log(wensite);
+}
+//console.log(username);
+
+//88888888888888888888888888888888888888888888888888888888888888 intersting
+addone(5)
+function addone(num){
+    return num+1;
+}
+addTwo(5)
+const  addTwo=function(num){
+    return num+2;
+}
+
+ 
+
+ 
