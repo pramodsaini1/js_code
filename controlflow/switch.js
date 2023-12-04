2e002c727e2d269b8d12fd538a2895c10b22f8f6
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-04 01:14:14 -0800

    this is concept of the control flow in switch case

---
+++ controlflow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month="march"
+switch (month) {
+    case  "jan":
+        console.log("january");
+        break;
+    case  "feb":
+        console.log("feb");
+        break;
+    case  "march":
+            console.log("mar");
+            break;
+    case  "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
