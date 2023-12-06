4ab2e294bf39f33f32243a7d2531aaecac7bd1cd
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-06 02:54:10 -0800

    this is concept of while and do-while loop

---
+++ controlflow/Iteration/2.js
@@ -0,0 +1,19 @@
+ //while loop
+//  let index=0;
+//  while (index<=10) {
+//     console.log(`value of index:${index}`);
+//     index +=2;
+//  }
+//  let myArray=["flash","batman","sueraman"]
+//  let arr=0;
+//  while(arr<myArray.length){
+//     console.log(`value is:${myArray[arr]}`);
+//     arr++;
+//  }
+
+ let score=11
+ do {
+    console.log(`score is:${score}`);
+    score++;
+    
+ } while (score<=10);
