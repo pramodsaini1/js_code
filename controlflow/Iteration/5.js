0a1b4514dcfcaa89a4faa138c90fdd577ee7ab23
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-07 02:13:44 -0800

    this is cocept about the loop

---
+++ controlflow/Iteration/5.js
@@ -0,0 +1,35 @@
+const coding=["js","ruby","java","python","cpp"]
+
+// coding.forEach( function   (item){
+//     console.log(item);
+// })
+
+// coding.forEach( (item)=>{
+//     console.log(item);
+// })
+
+function printMe(item){
+    console.log(item);
+}
+//coding.forEach(printMe)
+// coding.forEach( (item,index,arr)=>{
+//     console.log(item,index,arr);
+// })
+
+const myCoding=[
+    {
+        languagename:"javascript",
+        languageFileName:"js"
+    }
+    ,
+    
+    {
+
+    },
+    {
+
+    }
+]
+myCoding.forEach( (item)=>{
+    console.log(item.languagename);
+})
