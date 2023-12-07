445a5387ef6ee67e2b56106e50d98660d2c53978
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-07 01:55:09 -0800

    this is concept of the for in loop

---
+++ controlflow/Iteration/4.js
@@ -0,0 +1,23 @@
+const myObject={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift'
+}
+for (const key in  myObject) {
+    //console.log(`${key} sortcut is for ${myObject[key]}`);
+}
+const program=["js","java","cpp"]
+
+for (const key in  program) {
+    // console.log(program[key]);
+}
+// const map=new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+// map.set('Fr',"France")
+// map.set('IN',"India")
+
+// for (const key in  map) {
+//      //console.log(key);
+// }
