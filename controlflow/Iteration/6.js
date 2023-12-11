15c85ac58763cb7c9389dd1a82821f80b5941503
pramodsaini <pramodsaini25062000@gmail.com>
2023-12-11 01:08:55 -0800

    this is concept of the filter

---
+++ controlflow/Iteration/6.js
@@ -0,0 +1,29 @@
+const coding=["js","ruby","java","python","cpp"]
+
+// const values=coding.forEach( (item)=>{
+//     //console.log(item);
+//     return item
+// })
+// console.log(values);
+
+// const myNums=[1,2,3,4,5,6,7,8,9]
+// newNums= myNums.filter( (num)=>{
+//    return num>4
+// } )
+
+// const newNums=[]
+
+// newNums.forEach( (num)=>{
+//     if(nums>4){
+//        newNums.push(num)
+//     }
+// })
+
+
+
+// console.log(newNums);
+
+
+
+
+
