24cebdda18f273fb73cad815a1b26687b8233af2
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-03 02:29:01 -0800

    same

--- Api/promise.js
+++ Api/promise.js
@@ -55,5 +55,35 @@ const promiseTwo= new Promise(function(resolve,reject){
           reject('ERROR js Something went to wrong')
        }
   },1000)
- })
- promiseFive.then()
+ });
+ 
+  async function consumePromiseFive(){
+       try{
+            const response= await promiseFive
+            console.log(response);
+       }
+       catch(error){
+           console.log(error);
+       }
+  }
+  consumePromiseFive()
+
+//   async  function getAllUsers(){
+//      try{
+//            const response=  await fetch('https://jsonplaceholder.typicode.com/users')
+           
+//           const data=await response.json()
+//          console.log(data);
+//      }
+//      catch(error){
+//          console.log("E",error);
+//      }
+//   }
+//   getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users').then((response)=>{
+   return response.json()
+})
+.then((data)=>{
+   console.log(data);
+}).catch((error)=> console.log(error))
