2152d72eb80afed0deb0ba9468d57d17bca52e2a
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-11 01:26:00 -0800

    this is access the getter and setter through the Object

---
+++ oops/object_get_set.js
@@ -0,0 +1,13 @@
+const User={
+    _email:'h@hc.com',
+    _password:"123",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+    set email(value){
+          this._email=value
+    }
+}
+const tea= Object.create(User)
+console.log(tea.email);
