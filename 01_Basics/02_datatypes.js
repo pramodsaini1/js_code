20ee831052026f21ec83a5202f03dfdda9618283
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-26 02:23:38 -0800

    this is concept about the datatype in the js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+let name="oramod"
+let age=18
+let isLoggedIn=true
+
+
+//number=
+//bigInt
+//string---""
+//boolean----true/false
+//null-----standard value
+//undefined
+//symbol------unque
+
+
+
+
+//object
+
+
+console.log(typeof undefined);//undefined
+
+console.log(typeof null);//object
