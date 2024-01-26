86cce0dd1d1b354e27d9ab1a32e9acd0642449cc
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-26 02:11:45 -0800

    this is concept about the variable and thrie scopes

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=234323
+let accountEmail="pramod@gmail.com"
+var accountPassword="123"
+accountCity="jaipur"
+
+// accountId=2   can't change the const variable
+ 
+accountEmail="hr@gmail.com"
+accountPassword="12121213"
+accountCity="Bangloare"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+// can't prefer the var variable----bacause block and functional scope
