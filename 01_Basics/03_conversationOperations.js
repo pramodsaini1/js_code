c20c2209d60f070f6e44c883fee3f115b2d2c5ac
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-27 02:29:52 -0800

    this is convert the one data type to another datatype

---
+++ 01_Basics/03_conversationOperations.js
@@ -0,0 +1,12 @@
+let score="33abc"
+
+console.log(typeof score);//string
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);//number
+console.log(valueInNumber);//NaN
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
