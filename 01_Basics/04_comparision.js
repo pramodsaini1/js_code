fa3b80503a82377e7512193627cac31def4e0fc7
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-27 02:59:06 -0800

    compare

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,26 @@
+let value=3
+let negValue=-value
+console.log(negValue);
+
+let str1="Hello"
+let str2="World"
+let str3=str1+str2
+console.log(str3);
+
+console.log(2+"3");
+console.log(3+7+"10");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+console.log("2">3);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+
+console.log(undefined==0);
+
+console.log("2"===2);
