1002272a4b669606dbb143d26f0621359cc80400
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-29 02:04:39 -0800

    String concept

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,19 @@
+const name="pramod"
+const repocount=50
+
+console.log(name+repocount);
+
+ const gameName=new String("pramodsaini")
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('t'));
+
+ const newString=gameName.substring(0,4);
+ console.log(newString);
+
+ const anotherString="          pramod                   "
+
+ console.log(anotherString);
+
+ console.log(anotherString.trim());
