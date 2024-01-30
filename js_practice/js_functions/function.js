556430b4ef3e3affb76137144414d88772e85090
pramodsaini <pramodsaini25062000@gmail.com>
2024-01-30 03:49:51 -0800

    functions concept

---
+++ js_practice/js_functions/function.js
@@ -0,0 +1,15 @@
+//js functions is executed when invoked or call
+
+function myFunction(p1,p2){
+    return p1*p2;
+}
+console.log(myFunction(2,3));
+
+let myFunction = function(a,b){
+    return a*b;
+}
+console.log(myFunction(5,10));
+
+// why functions ------------------------with function reuse the code-----you can write the code and many times used
+
+
